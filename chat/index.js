56d5f504135e46fcc8f24aece1f501d0638d13c0
J-Pard <pardons.jonathan@gmail.com>
2020-06-05 10:27:55 +0200

    Chat init : Express + Socket

---
+++ chat/index.js
@@ -0,0 +1,30 @@
+const HTTP = require('http');
+const EXPRESS = require('express');
+const PORT = 5000;
+
+const APP = EXPRESS();
+const SERVER = HTTP.createServer(APP);
+
+// RUNNING
+SERVER.listen(PORT, () => {
+      console.log("Awesome Chat is listening...");
+});
+
+// ROUTING
+
+APP.get("/", (req, res) => {
+      res.setHeader("Content-Type", "text/html; charset=utf-8");
+      res.render('index.ejs');
+})
+
+.use((req, res) => {
+      res.setHeader("Content-Type", "text/html; charset=utf-8");
+      res.status(404).send("Nothing was found here.");
+});
+
+// SOCKET
+
+const IO = require('socket.io').listen(SERVER);
+IO.sockets.on('connection', (socket) => {
+      console.log("New user has arrived.");
+});
